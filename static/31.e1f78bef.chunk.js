fa275f8e5e0d83d0052482fce6336b1dd81a05d8
Tep Tek <teptek@gmail.com>
2018-07-09 16:35:22 +0200

    bugs fixed & icons added

---
+++ static/31.e1f78bef.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([31],{808:function(n,e,t){var o=t(15),a=t(45),i=t(289).PageRenderer;i.__esModule&&(i=i.default);var r=a({displayName:"WrappedPageRenderer",getInitialState:function(){return{content:t(840)}},componentWillMount:function(){},render:function(){return o.createElement(i,Object.assign({},this.props,{content:this.state.content}))}});r.__catalog_loader__=!0,n.exports=r},840:function(n,e){n.exports='```html\nshowSource: 1\n---\n\n<div class="ico-bg">\n    <i class="ico-validation ico-green"></i>\n</div>\n\n\n```\n'}});
+//# sourceMappingURL=31.e1f78bef.chunk.js.map
