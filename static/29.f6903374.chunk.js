98352d7fdfe531f042e51baebe7b50c68eeb47f3
Tep Tek <teptek@gmail.com>
2018-07-25 16:55:34 +0200

    radio added

--- static/29.f6903374.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([29],{806:function(n,e,t){var o=t(15),a=t(45),i=t(289).PageRenderer;i.__esModule&&(i=i.default);var r=a({displayName:"WrappedPageRenderer",getInitialState:function(){return{content:t(836)}},componentWillMount:function(){},render:function(){return o.createElement(i,Object.assign({},this.props,{content:this.state.content}))}});r.__catalog_loader__=!0,n.exports=r},836:function(n,e){n.exports='```html\nshowSource: 1\n---\n\n<div class="ico-bg">\n    <i class="ico-validation ico-green"></i>\n</div>\n\n\n```\n'}});
-//# sourceMappingURL=29.f6903374.chunk.js.map
